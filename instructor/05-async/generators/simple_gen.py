9b30e7300bf22b3881c3f88fdcd5afeebc89c7aa
Michael Kennedy <mikeckennedy@gmail.com>
2022-07-07 16:12:52 -0700

    Prepare generator code

--- instructor/05-async/generators/simple_gen.py
+++ instructor/05-async/generators/simple_gen.py
@@ -1,17 +1,2 @@
-def fib(n: int) -> list[int]:
-    numbers = []
-    current, nxt = 0, 1
-    while len(numbers) < n:
-        current, nxt = nxt, current + nxt
-        numbers.append(current)
-
-    return numbers
-
-
-result = fib(5)
-
-for n in result:
-    print(n, end=', ')
-
-print()
-print("Done")
+# TODO: Implement basic fib via lists
+# TODO: Implement fib via generators
